a2f2f5aea87fba0648d0d372310d8d2556287d94
Brian Lego <legobw@gmail.com>
2020-07-07 16:25:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,9 +59,9 @@ end
 #####################################################
 
 def runner
-  total = 0 
+
   welcome
-  total += initial_round
+  total = initial_round
   #binding.pry
   until total > 21
     total += hit?(total)
