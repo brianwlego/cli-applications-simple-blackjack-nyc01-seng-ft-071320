127a9773da1327563707a98f8f96aa5bb23f2f66
Brian Lego <legobw@gmail.com>
2020-07-07 15:40:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,7 +51,7 @@ def hit?(current_card_total)
 end
 
 def invalid_command
-  p "Please enter a valid command"
+  puts "Please enter a valid command"
 end
 
 #####################################################
